568ec39ff12f79a1aaf0c9a29c16864d6c58d79b
Emil Karlsson <own.emka@gmail.com>
2024-03-18 15:01:58 +0100

    Improve Virtualization section and add more figures

--- report/scripts/fetch_figures.py
+++ report/scripts/fetch_figures.py
@@ -5,24 +5,32 @@ import pdf2image
 
 permalink = "https://docs.google.com/presentation/d/1RgcRZENtoHmxv5kiTlNwwcNEDgpMYzRaJOtbawGU5Vs/export?format=pdf"
 first_slide = 2
-last_slide = 8
+last_slide = -1
 pdf_local_name = "figures.pdf"
 out_dir = "../img"
 
 
 
 def fetch_pdf():
-    urllib.request.urlretrieve(permalink, pdf_local_name)
+    urllib.request.urlretrieve(permalink, os.path.join("out" ,pdf_local_name))
 
 
 def main():
+    # Create dir out if not exists
+    if not os.path.exists("out"):
+        os.makedirs("out")
+
     print("Fetching figures...")
     fetch_pdf()
 
     print("Extracting figures...")
-    reader = PdfReader(pdf_local_name)
+    reader = PdfReader(os.path.join("out" ,pdf_local_name))
 
     # The pdf should be structured as text>figure>text>figure>text>figure
+
+    global last_slide
+    if last_slide == -1:
+        last_slide = len(reader.pages)
     
     for i in range(last_slide - first_slide):
         if i % 2 == 0:
@@ -37,13 +45,8 @@ def main():
             # Then use imagemagick to convert to png
             writer = PdfWriter()
             writer.add_page(reader.pages[first_slide + i])
-
-            # Create dir out if not exists
-            if not os.path.exists("out"):
-                os.makedirs("out")
             
             writer.write("out/figure" + str(i) + ".pdf")
-            print("Wrote figure" + str(i) + ".pdf")
 
             images = pdf2image.convert_from_path("out/figure" + str(i) + ".pdf")
             images[0].save("out/figure" + str(i) + ".png", "PNG")
@@ -52,9 +55,8 @@ def main():
             output_file = os.path.join(out_dir, filename + ".png")
             os.system("convert out/figure" + str(i) + ".png -trim " + output_file)
 
-            # Remove temporary pdf and png
-            os.remove("out/figure" + str(i) + ".pdf")
-            os.remove("out/figure" + str(i) + ".png")
+    # Remove out folder
+    os.system("rm -rf out")
 
     print("Done")
 
